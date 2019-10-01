5668aed3f6288a0216eb48633eb50917bb1b084a
duy.ton <duy.ton@exe.com.vn>
2019-10-01 17:41:38 +0700

    rework again with pairBookingDates removed

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const debug = require('debug');
 var checking = debug('checking');
 
 // export main functions
-module.exports = function checkRooms(rooms, bookings) {
-    return checkAvailable(rooms, pairBookingDates(bookings));
-}
+module.exports = function checkRooms(rooms, [arrivals, departures] = [[], []]) {
 
-// module.exports.checkAvailable = checkAvailable;
-// module.exports.pairBookingDates = pairBookingDates;
-
-/**
- * preprocess data from required inputs
- * @param {string[][]} 2-d array of arrivals & depatures
- */
-function pairBookingDates([arrivals, departures] = [[], []]) {
+    if (!rooms) throw new Error('There\'s even no room!');
 
     if (!Array.isArray(arrivals) || !Array.isArray(departures)) {
         throw new Error('Booking data are not in right format!');
@@ -23,25 +14,21 @@ function pairBookingDates([arrivals, departures] = [[], []]) {
     if (arrivals.length != departures.length) {
         throw new Error('Booking arrivals & departures are not matched!');
     }
-    // map into numeric pairs of arrival-departure
-    return arrivals.map((value, i) => [+value, +departures[i]]);
+
+    return checkAvailable(rooms, arrivals, departures);
+
 }
 
-/**
- * check for longest, consecutive times of room
- * @param {number[][]} bookedData - loaded data from bookings.txt
- */
-function checkAvailable(rooms, bookedData) {
 
-    if (!rooms) throw new Error('There\'s even no room!');
-
+function checkAvailable(rooms, arrivals, departures) {
     // generate booking tracks for available rooms
     var roomTracks = [...Array(rooms)].map(() => ({ min: 0, max: 0 }));
-    var bLen = bookedData.length, bIdx = 0, rIdx = 0;
+    var bLen = arrivals.length, bIdx = 0, rIdx = 0;
+    var bArv = arrivals[bIdx], bDpt = departures[bIdx];
 
     while (bIdx < bLen) {
 
-        let [bArv, bDpt] = bookedData[bIdx], { min, max } = roomTracks[rIdx];
+        let { min, max } = roomTracks[rIdx];
 
         checking(`booking from day ${bArv} to day ${bDpt}...`);
 
@@ -59,6 +46,7 @@ function checkAvailable(rooms, bookedData) {
         }
 
         bIdx += 1, rIdx = 0; // reset room index & proceed to next booking
+        bArv = arrivals[bIdx], bDpt = departures[bIdx];
 
     }
 
